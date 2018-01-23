3c80a47a0c5d13644551beb4ccbf633281de9ba4
application <postmaster@photonshalo.com>
2018-01-23 11:31:16 +0800

    add

---
+++ pack_install.bat
@@ -0,0 +1,32 @@
+npm i webpack
+npm i react
+npm i react-dom -S
+npm i babel-loader -D
+npm i babel-core -D
+npm i babel-preset-es2015 babel-preset-es2015-loose babel-preset-react -D
+npm i html-webpack-plugin -D
+npm i rimraf -D
+npm i express -S
+npm i webpack-dev-server -D
+npm i cross-env -D
+npm i react-hot-loader@next -D
+npm i axios -S
+npm i memory-fs -D
+npm i http-proxy-middleware -D
+npm i eslint -D
+npm i babel-eslint eslint-config-airbnb eslint-config-standard eslint-loader eslint-plugin-import eslint-plugin-jsx-a11y eslint-plugin-node eslint-plugin-promise eslint-plugin-react eslint-plugin-standard -D
+npm i webpack-merge -D
+npm i serve-favicon -S
+npm i nodemon -D
+npm i react-router -S
+npm i react-router-dom -S
+npm i babel-plugin-transform-decorators-legacy babel-preset-stage-1 -D
+npm i mobx mobx-react -S
+npm i prop-types -S
+npm i body-parser express-session query-string -S
+npm i react-async-bootstrapper -S
+npm i ejs-compiled-loader ejs -S
+npm i serialize-javascript -S
+npm i react-helmet -S
+npm i material-ui@next material-ui-icons -S
+npm i react-jss jss jss-preset-default -S
